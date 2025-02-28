ff5afd0f7ec6f9fccf07cce40a0ee6d3c287cd81
Skybluewater <cuichengyuan@live.cn>
2025-02-28 16:35:16 +0800

    add jsonl and llm assist func

--- perform_dataset_process/utils/__init__.py
+++ perform_dataset_process/utils/__init__.py
@@ -2,6 +2,7 @@ from .text import extract_text_from_audio, align_chunks_with_timestamps
 from .video import split_video_frames_by_duration, extract_keyframes
 from .audio import extract_audio_from_video
 from .img import align_img_with_chunk, image_to_base64
+from .llm import format_llm_message, format_vlm_message
 
 # __init__.py
 
@@ -14,4 +15,6 @@ __all__ = [
     'extract_audio_from_video', 
     'align_img_with_chunk',
     'image_to_base64',
+    'format_llm_message',
+    'format_vlm_message',
 ]
